d9aa118bc5429ca3ca474c75533db8414f64d769
ALEKS MOLODECKIJ <molodeckij.aleks1998@gmail.com>
2022-01-28 16:37:54 +0200

    added sound effects

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,27 +1,37 @@
-import { useState } from 'react'
-import Card from './Card'
+import { useState } from 'react';
+import Card from './Card';
+import {Howl, Howler} from 'howler';
 
 function Cards(){
     const [items, setItems] = useState([
-        { id: 1, img: '/img/html.png', stat: "" },
-        { id: 1, img: '/img/html.png', stat: "" },
-        { id: 2, img: '/img/css.png', stat: "" },
-        { id: 2, img: '/img/css.png', stat: "" },
-        { id: 3, img: '/img/js.png', stat: "" },
-        { id: 3, img: '/img/js.png', stat: "" },
-        { id: 4, img: '/img/scss.png', stat: "" },
-        { id: 4, img: '/img/scss.png', stat: "" },
-        { id: 5, img: '/img/react.png', stat: "" },
-        { id: 5, img: '/img/react.png', stat: "" },
-        { id: 6, img: '/img/vue.png', stat: "" },
-        { id: 6, img: '/img/vue.png', stat: "" },
-        { id: 7, img: '/img/angular.png', stat: "" },
-        { id: 7, img: '/img/angular.png', stat: "" },
-        { id: 8, img: '/img/nodejs.png', stat: "" },
-        { id: 8, img: '/img/nodejs.png', stat: "" }
+        { id: 1, img: '/img/html.png', stat: "", sound: "/sound/1.wav" },
+        { id: 1, img: '/img/html.png', stat: "", sound: "/sound/1.wav" },
+        { id: 2, img: '/img/css.png', stat: "", sound: "/sound/2.wav" },
+        { id: 2, img: '/img/css.png', stat: "", sound: "/sound/2.wav" },
+        { id: 3, img: '/img/js.png', stat: "", sound: "/sound/3.wav" },
+        { id: 3, img: '/img/js.png', stat: "", sound: "/sound/3.wav" },
+        { id: 4, img: '/img/scss.png', stat: "", sound: "/sound/4.wav" },
+        { id: 4, img: '/img/scss.png', stat: "", sound: "/sound/4.wav" },
+        { id: 5, img: '/img/react.png', stat: "", sound: "/sound/5.wav" },
+        { id: 5, img: '/img/react.png', stat: "", sound: "/sound/5.wav" },
+        { id: 6, img: '/img/vue.png', stat: "", sound: "/sound/6.wav" },
+        { id: 6, img: '/img/vue.png', stat: "", sound: "/sound/6.wav" },
+        { id: 7, img: '/img/angular.png', stat: "", sound: "/sound/7.wav" },
+        { id: 7, img: '/img/angular.png', stat: "", sound: "/sound/7.wav" },
+        { id: 8, img: '/img/nodejs.png', stat: "", sound: "/sound/8.wav" },
+        { id: 8, img: '/img/nodejs.png', stat: "", sound: "/sound/8.wav" }
     ].sort(() => Math.random() - 0.5))
 
     const [prev, setPrev] = useState(-1)
+    const {Howl, Howler} = require('howler')
+    const soundPlay = (src) =>{
+        const sound = new Howl({
+            src
+        })
+        sound.play();
+
+    }
+
 
     function check(current){
         if(items[current].id == items[prev].id){
@@ -47,8 +57,10 @@ function Cards(){
             items[id].stat = "active"
             setItems([...items])
             setPrev(id)
+            soundPlay(items[id].sound)
         }else{
             check(id)
+            soundPlay(items[id].sound)
         }
     }
 
